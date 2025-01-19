fc172ba056268c32c2f0558913f5275ac514cf98
Shiva <gupta4002001@gmail.com>
2025-01-19 15:23:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'; // Assuming you need this logo for some purpose
+import './App.css';
+import FirstPage from './components/firstPage'; // Correct import for a default-exported React component
+
+function App() {
+  return (
+    <>
+      <FirstPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/firstPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './firstPage.css'; // Import external CSS for styling
+
+export default function FirstPage() {
+  return (
+    <div className="parent">
+      <div className="firstbox">Hello, I'm centered!</div>
+    </div>
+  );
+}
