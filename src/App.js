1af5db4632ff7cd25a475e7681b8843bb258e846
shiva676 <75204180+shiva676@users.noreply.github.com>
2025-01-19 15:38:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'; // Assuming you need this logo for some purpose
 import './App.css';
 import FirstPage from './components/firstPage'; // Correct import for a default-exported React component
 
