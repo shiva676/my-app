4a034ef53e11a2e4549f9e263a2857a2108228ee
shiva676 <75204180+shiva676@users.noreply.github.com>
2025-01-19 15:40:51 +0530

    Update firstPage.js

--- src/components/firstPage.js
+++ src/components/firstPage.js
@@ -4,7 +4,7 @@ import './firstPage.css'; // Import external CSS for styling
 export default function FirstPage() {
   return (
     <div className="parent">
-      <div className="firstbox">Hello, I'm centered!</div>
+      <div className="firstbox">Hello, I'm tered!</div>
     </div>
   );
 }
